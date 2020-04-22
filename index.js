d5059c664217c0aec9af2d839bbebed1569cfe8c
Aristos Markogiannakis <aristosmarkogiannakis@aristoss-mbp.home>
2020-04-22 23:36:07 +0100

    added

--- index.js
+++ index.js
@@ -2,10 +2,32 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: 'World'}));
 });
+
+
+app.get('/api/stores', (req, res) => {
+    const {
+        query: {missing },
+        method,
+    } = req
+
+    switch (method) {
+        case 'GET':
+            res.send(JSON.stringify({ Hello: 'GET'}));
+            break;
+        case 'POST':
+            res.send(JSON.stringify({ Hello: 'POST'}));
+            break;
+        default:
+        res.setHeader('Allow', ['GET', 'PUT'])
+        res.status(405).end(`Method ${method} Not Allowed`)
+    }
+});  
 
 app.listen(port, function () {
  console.log('Example app listening on port !');
